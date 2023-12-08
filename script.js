3566145bcb8d12feedda69e911f64f331ed6bc32
Célelo <mariaceleste.lopez41@gmail.com>
2023-12-08 00:06:44 -0300

    incorporo seccion operaciones hecha y carpeta de imagenes creada

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const $ = (selector) => document.querySelector(selector)
 $('#burger-btn').addEventListener('click', () => {
     $('#burger-menu').classList.toggle('hidden');
 });
+
+// ocultar filtros
+$('#hiddenFilters').addEventListener('click', () => {
+    $('.section-filters').style.height = '20vh'
+    $('.filters').classList.toggle('hidden')
+    $('#hiddenFilters').classList.toggle('hidden')
+    $('#haddenFilters').classList.toggle('hidden')
+})
+ // mostrar filtros
+$('#haddenFilters').addEventListener('click', () => {
+    $('.section-filters').style.height = '63vh'
+    $('.filters').classList.toggle('hidden')
+    $('#hiddenFilters').classList.toggle('hidden')
+    $('#haddenFilters').classList.toggle('hidden')
+})
