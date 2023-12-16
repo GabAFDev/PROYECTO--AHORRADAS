364bc123d497f82bbc8e20d7ca294e208b6f29ec
Célelo <mariaceleste.lopez41@gmail.com>
2023-12-15 21:45:37 -0300

    incorporo operaciones guardadas en el localStorage

--- script.js
+++ script.js
@@ -13,18 +13,13 @@ $('#btnNewOperation').addEventListener('click', () => {
     $('.new-operarion-screen').classList.toggle('hidden')
 })
 
-<<<<<<< HEAD
 // cancelar nueva operacion
-=======
->>>>>>> 9402b662107b571c14e47080a74b6bcc1990c4b1
 $('#cancelButtonNo').addEventListener('click', () => {
     $('.balance-screen').classList.toggle('hidden')
     $('.new-operarion-screen').classList.toggle('hidden')
 })
-<<<<<<< HEAD
-=======
 
->>>>>>> 9402b662107b571c14e47080a74b6bcc1990c4b1
+
 // ocultar filtros
 $('#hiddenFilters').addEventListener('click', () => {
     $('.section-filters').style.height = '20vh'
@@ -32,10 +27,66 @@ $('#hiddenFilters').addEventListener('click', () => {
     $('#hiddenFilters').classList.toggle('hidden')
     $('#haddenFilters').classList.toggle('hidden')
 })
- // mostrar filtros
+// mostrar filtros
 $('#haddenFilters').addEventListener('click', () => {
     $('.section-filters').style.height = '63vh'
     $('.filters').classList.toggle('hidden')
     $('#hiddenFilters').classList.toggle('hidden')
     $('#haddenFilters').classList.toggle('hidden')
 })
+
+
+
+
+
+// SECCION AGREGAR NUEVA OPERACION 
+const newOperation = () => {
+    return {
+        description: $('#descriptionNo').value,
+        amount: $('#amountNo').value,
+        type: $('#typeSelect').value,
+        category: $('#inputCategories').value,
+        date: $('#inputDate').value
+    };
+}
+
+const dates = {
+    operations: [],
+    categories: []
+}
+
+const button = $('#addButtonNo');
+
+
+button.addEventListener('click', () => {
+    // const operation = newOperation()
+
+    const { operations } = dates
+
+    const operation = newOperation();
+    if (!isOperationDuplicate(operation, operations)) {
+        operations.push(operation);
+        console.log(dates);
+
+        // lo volvemos un objeto json y lo seteamos al localStorage
+        const parsedDates = JSON.stringify(dates);
+        localStorage.setItem('dates', parsedDates);
+    } else {
+        alert('La operación ya existe y no se puede repetir.');
+    }
+})
+
+
+
+
+// funcion que verifica si la operacion quiere ser duplicada 
+
+function isOperationDuplicate(newOperation, operations) {
+    // compara la operacion a agregar con la ya existente en el localStorage
+    const duplicateOperations = operations.filter(existingOperation => {
+        return JSON.stringify(existingOperation) === JSON.stringify(newOperation);
+    });
+
+    // Si hay operaciones duplicadas, el tamaño de duplicateOperations será mayor que cero
+    return duplicateOperations.length > 0;
+}
