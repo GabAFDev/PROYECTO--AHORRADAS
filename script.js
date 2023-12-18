bb7ce6858fcbdca57e6ec8d3939303adbdfb6af7
Célelo <mariaceleste.lopez41@gmail.com>
2023-12-18 02:57:37 -0300

    incorporo operaciones en pantalla traidas del localStorage

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 // FUNCIONES REUTILIZABLES
 const $ = (selector) => document.querySelector(selector)
 
@@ -16,15 +17,67 @@ const add = (selectors) => {
 const randomId = () => self.crypto.randomUUID()
 
 // funciones localStorage
+
+// enviar datos
 const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data))
 // pedir info
 const getData = (key) => JSON.parse(localStorage.getItem(key))
 
+// cuando se inicialice va aguardar toda las operaciones de (trae la info del localStorage)
+const allOperation = getData('operations') || [] // que me traiga la info del local, pero si no hay, que traiga un array vacio
 
 
-// FUNCIONALIDAD DE BOTONES
 
-const initializacionButtons = () => {
+
+
+
+
+
+
+
+// SECCION AGREGAR NUEVA OPERACION 
+
+
+
+
+const iterateOperations = (operations) => {
+    for (const operation of operations) {
+        $('#tableOperations').innerHTML += `
+        <tr class="border-b">
+                    <td class="p-4">${operation.description}</td>
+                    <td class="mt-6 ml-5 py-1 px-2 inline-block bg-[#886a8e]  rounded-full">${operation.category}</td>
+                    <td class="p-2">${operation.date}</td>
+                    <td class="p-2">${operation.amount}</td>
+                    <td class="p-2 flex flex-col space-y-2">
+                        <button class="text-[12px] text-green-300 hover:text-slate-500">Editar</button>
+                        <button class="text-[12px] text-red-300 hover:text-slate-500">Eliminar</button>
+                    </td>
+                </tr>
+    `
+    }
+}
+
+
+
+//guarda el value de los inputs como objetos
+const infoForm = () => {
+    return {
+        id: randomId(),
+        description: $('#descriptionNo').value,
+        amount: $('#amountNo').value,
+        type: $('#typeSelect').value,
+        category: $('#inputCategories').value,
+        date: $('#inputDate').value
+    };
+}
+
+
+// EVENTOS
+const initializacion = () => {
+    // en operations envia allOperation (este tiene todas las operaciones realizadas almacenadas)
+    setData('operations', allOperation)
+    // ambes de iniciar  renderOperations abajo de la misma , la inicializamos aca con las operaciones ya obtenidas y parseadas del localStorage
+    iterateOperations(allOperation)
 
     // menu hamburguesa
     $('#burger-btn').addEventListener('click', () => {
@@ -55,58 +108,25 @@ const initializacionButtons = () => {
         remove(['.filters', '#hiddenFilters'])
         add(['#haddenFilters'])
     })
+
+
+    $('#addButtonNo').addEventListener('click', () => {
+        // guardamos los valores de los inputs en una costante
+        // const newOperation = saveOperationInfo()
+        // setData('operations', newOperation)
+
+        // pasos antes de iterar rn psntalla:
+        //1. te volves a traer la info actualizada del local (se vuelve en formato array por que getData lo pasea)
+        const updatedtData = getData('operations')
+        // 2. pusheas la funcion saveOperationInfo() que tiene todos los values del formulario
+        updatedtData.push(infoForm())
+        // 3. ahora ya modificado ahora si se puede mandar a setData el cual lo introduce al localStorage
+        setData('operations', updatedtData) // haciendo estos pasos metimos al objeto adentro de un array para poder ser iterado
+    })
+
 }
 
-initializacionButtons()
-
-
-
-
-
-
-
-// SECCION AGREGAR NUEVA OPERACION 
-
-const newOperation = () => [
-    {
-        id: 1,
-        description: 'super',
-        category: 'compras',
-        date: '6 / 11 / 2023',
-        amount: 30000
-    },
-
-    {
-        id: 2,
-        description: 'HELADO',
-        category: 'salida',
-        date: '6 / 11 / 2023',
-        amount: 2000
-    }
-]
-
-
-const renderOperations = (operations) => {
-    for (const operation of operations) {
-        $('#tableOperations').innerHTML += `
-        <tr class="border-b">
-                    <td class="p-4">${operation.description}</td>
-                    <td class="mt-6 ml-5 py-1 px-2 inline-block bg-[#886a8e]  rounded-full">${operation.category}</td>
-                    <td class="p-2">${operation.date}</td>
-                    <td class="p-2">${operation.amount}</td>
-                    <td class="p-2 flex flex-col space-y-2">
-                        <button class="text-[12px] text-green-300 hover:text-slate-500">Editar</button>
-                        <button class="text-[12px] text-red-300 hover:text-slate-500">Eliminar</button>
-                    </td>
-                </tr>
-    `
-    }
-}
-
-renderOperations(newOperation())
-
-
-
+window.addEventListener('load', initializacion())
 
 
 
